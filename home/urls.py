cbbc0a325f6dc3aa94130bcb831c6d4ab8e7e23a
ritik7jain <jritik324@gmail.com>
2023-08-06 01:48:17 +0530

    urls updated

--- home/urls.py
+++ home/urls.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 from django.urls import path,include
 from home import views
 urlpatterns = [
-    path('', views.signIn),
-    path('signUp/', views.signUp, name="signup"),
-    path('logout/', views.logout, name="log"),
+    path('', views.index),
+    path('signUp/', views.signUp),
+    path('logout/', views.logout),
     path('reset/', views.reset),
-    path('home/',views.home),
-    path('index/',views.index),
+    path('signin/',views.signIn),
     path('upload_pdf/',views.upload_pdf),
     path('printout/',views.services),
     path('admin_login/',views.admin_login),
@@ -17,3 +16,19 @@ urlpatterns = [
     path('contact/',views.contact),
     path('order_details/', views.order_detail)
 ]
+# urlpatterns = [
+#     path('', views.signIn),
+#     path('signUp/', views.signUp, name="signup"),
+#     path('logout/', views.logout, name="log"),
+#     path('reset/', views.reset),
+#     path('home/',views.home),
+#     path('index/',views.index),
+#     path('upload_pdf/',views.upload_pdf),
+#     path('printout/',views.services),
+#     path('admin_login/',views.admin_login),
+#     path('admin_home/',views.admin_home),
+#     path('order/',views.orders),
+#     path('faqs/',views.faqs),
+#     path('contact/',views.contact),
+#     path('order_details/', views.order_detail)
+# ]
