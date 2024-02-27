b876d91c518452fcd8ecedecc596de99832d2567
ritik7jain <jritik324@gmail.com>
2024-02-27 21:43:44 +0530

    corrected static url

--- printing/settings.py
+++ printing/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/css'
+STATIC_URL = '/static/css/'
 # if not DEBUG:
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/css')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/css/')
 # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
 # and renames the files with unique names for each version to support long-term caching
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
