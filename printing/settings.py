3590a4efb7286b6a008d2a7b318defc1a2d25178
ritik7jain <jritik324@gmail.com>
2024-02-27 23:04:02 +0530

    corrected static url

--- printing/settings.py
+++ printing/settings.py
@@ -132,6 +132,7 @@ if not DEBUG:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #added
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static")
 ]
