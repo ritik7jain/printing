d09746d6f1c4739099857de439b7dcc530d36994
ritik7jain <jritik324@gmail.com>
2024-02-27 22:48:17 +0530

    debug true

--- printing/settings.py
+++ printing/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1","192.168.1.103","localhost","https://printing-3upw.onrender.com/","*"]
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -118,13 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/css/'
-# if not DEBUG:
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/css/')
-# Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-# and renames the files with unique names for each version to support long-term caching
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+if not DEBUG:
+    PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
